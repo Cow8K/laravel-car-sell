0f72accbee61e65abd7ed7160e0a2697ccd26509
Liu Sheng <894349920@qq.com>
2026-01-22 22:24:36 +0800

    Init Project

---
+++ resources/views/about.blade.php
@@ -0,0 +1,2 @@
+<h1>Hello Welcome to my Laravel course</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet architecto autem dicta, inventore natus nobis reprehenderit repudiandae! Aperiam culpa dolorem facilis illum iure magnam nulla, praesentium reiciendis sed sunt, vel?</p>
