cbbe57089e8e515fe1ff1ae437d4ba3cc82de41d
Liu Sheng <894349920@qq.com>
2026-02-04 14:59:27 +0800

    feat: 创建注册界面

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SignupController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/signup', [SignupController::class, 'create']);
 Route::get('/home', [HomeController::class, 'index'])->name('home');
